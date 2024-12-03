3356a65f6b0874f3f41ca90c878e1de2b819741b
karoll1naa <karollinnajan@gmail.com>
2024-12-03 20:29:43 +0200

    Rewrote Main, which does not show the work of lab 5, but shows the work of the created constructors

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,40 +1,24 @@
 package Main;
 
 import Gemstone.*;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            CollectionSet<Stone> gemstoneSet = new CollectionSet<Stone>();
-            gemstoneSet.add(new Gemstone("Сапфір", 2.5, 7000, 0.85));
-            gemstoneSet.add(new SemiPreciousStone("Смарагд", 1.0, 4000, 0.9));
-            gemstoneSet.add(new SemiPreciousStone("Опал", 1.8, 2000, 0.6));
+        CollectionSet<Stone> gemstoneSet = new CollectionSet<Stone>();
+        gemstoneSet.add(new Gemstone("Рубін", 1.2, 3000, 0.8));
+        gemstoneSet.add(new SemiPreciousStone("Діамант", 1.5, 5000, 0.95));
+        gemstoneSet.add(new SemiPreciousStone("Аметист", 2.0, 1500, 0.7));
 
-            System.out.println("Камені в наборі: ");
-            System.out.println(gemstoneSet);
+        //Виведення усієї колекції
+        System.out.println("Камені в наборі: ");
+        System.out.println(gemstoneSet);
 
-            Necklace necklace = new Necklace();
-            necklace.addStone(new Gemstone("Сапфір", 2.5, 7000, 0.85));
-            necklace.addStone(new Gemstone("Смарагд", 1.0, 4000, 0.9));
-            necklace.addStone(new SemiPreciousStone("Опал", 1.5, 2000, 0.6));
+        //Перевірка конструктора
+        CollectionSet<Stone> singleStoneSet = new CollectionSet<>(new Gemstone("Сапфір", 2.5, 7000, 0.85));
+        System.out.println("\nНабір з одного каменя: " + singleStoneSet);
 
-            System.out.println("\nЗагальна вага намиста: " + necklace.calculateWeight() + " каратів");
-            System.out.println("Загальна вартість намиста: " + necklace.calculateCost());
-
-            System.out.println("\nДо сортування:");
-            System.out.println(necklace);
-            necklace.sortByCost();
-            System.out.println("\nПісля сортування:");
-            System.out.println(necklace);
-
-            System.out.println("\nКамені з прозорістю від 0.75 до 1.0:");
-            List<Stone> filteredStone = necklace.findByTransparency(0.75, 1.0);
-            filteredStone.forEach(System.out::println);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Помилка: " + e.getMessage());
-        } catch (Exception e) {
-            System.err.println("Непередбачена помилка: " + e.getMessage());
-        }
+        //Використання колекції для ініціалізації
+        CollectionSet<Stone> collectionSet = new CollectionSet<>(gemstoneSet);
+        System.out.println("\nНабір, створений з іншої колекції: " + collectionSet);
     }
 }
